f0de905ea5483aea89dd5f102a3484970ae7a067
aliali <aliali@test.com>
2025-04-21 15:02:16 +0300

    first

---
+++ src/main/java/com/example/sakanmate/SakanMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.sakanmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakanMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SakanMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sakanmate/SakanMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sakanmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakanMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
