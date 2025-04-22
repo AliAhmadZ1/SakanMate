79e6e7d02e6bb22736d26a13dea47f42406fb1b9
aliali <aliali@test.com>
2025-04-22 10:22:32 +0300

    add get all method

--- src/main/java/com/example/sakanmate/Service/RequestService.java
+++ src/main/java/com/example/sakanmate/Service/RequestService.java
@@ -1,13 +1,20 @@
 package com.example.sakanmate.Service;
 
+import com.example.sakanmate.Model.Request;
 import com.example.sakanmate.Repository.RenterRepository;
 import com.example.sakanmate.Repository.RequestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class RequestService {
     private final RequestRepository requestRepository;
+
+    public List<Request> getAll(){
+        return requestRepository.findAll();
+    }
 }
 
